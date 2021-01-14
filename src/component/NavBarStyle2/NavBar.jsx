e48d2ef4f24bcb2df46f551d8d504012687396c4
Osh <osh.pro@outlook.fr>
2021-01-14 04:00:36 +0100

    navbar rework

---
+++ src/component/NavBarStyle2/NavBar.jsx
@@ -0,0 +1,24 @@
+// import React, { useEffect, useState } from "react";
+// import "./NavBar.css";
+
+// const NavBar = function({button, anchor, logo}) {
+
+//   const [slotState, set] = useState([]);
+
+
+  
+//   return (
+//     <header>
+
+//     <div className="logo" alt="logo"></div>
+//     <ul className="navbar">
+//       <li></li>
+//       <li></li>
+//       <li></li>
+//       <li></li>
+//     </ul>
+//     </header>
+//   );
+// }
+
+// export default NavBar;
--- src/page/App.jsx
+++ src/page/App.jsx
@@ -7,26 +7,31 @@ const App = function() {
   const navBar1 = {
     "_logo": "#",
     "_burger": "#",
-    "M Pomme de terre": "#",
-  }
-
-  const navBar2 = {
+    "Meta Pomme de terre": "#",
     "Place des ventes": "#",
     "Blog": "#",
     "Contact": "#",
-  }
-
-  const navBar3 = {
     "Donation": "#",
     "_shop": "#",
   }
+
+  // const navBar2 = {
+  //   "Place des ventes": "#",
+  //   "Blog": "#",
+  //   "Contact": "#",
+  // }
+
+  // const navBar3 = {
+  //   "Donation": "#",
+  //   "_shop": "#",
+  // }
   
   return (
     <div id="app">
       <header>
         <NavBar slotList={generateAnchor(navBar1)}/>
-        <NavBar slotList={generateAnchor(navBar2)}/>
-        <NavBar slotList={generateAnchor(navBar3)}/>
+        {/* <NavBar slotList={generateAnchor(navBar2)}/>
+        <NavBar slotList={generateAnchor(navBar3)}/> */}
       </header>
       <main></main>
     </div>
