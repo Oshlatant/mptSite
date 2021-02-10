dd16f32d3a88206551b31d837e096379230b8aa0
Osh <osh.pro@outlook.fr>
2021-02-10 21:21:33 +0100

    carousel 80% done

--- src/component/Carousel/Carousel.jsx
+++ src/component/Carousel/Carousel.jsx
@@ -5,7 +5,7 @@ import "./Carousel.css"
 const Carousel = () => {
 
   const [targetIMG, setTargetIMG] = useState(0);
-  const [imageList, setImageList] = useState(["https://via.placeholder.com/320x290"]);
+  const [imageList, setImageList] = useState(["https://via.placeholder.com/320x180", "https://i.imgur.com/xq3Gvdh.jpg", "https://i.imgur.com/zn44Arr.jpeg", "https://i.imgur.com/d2KSrH1.jpg"]);
   
   // console.log(background);
   const style = {
@@ -13,12 +13,18 @@ const Carousel = () => {
     // backgroundColor: "red",
   }
 
+  const swapImage = function(indice) {
+    const targetIndex = targetIMG + indice;
+
+    setTargetIMG(targetIndex >= 0 && targetIndex < imageList.length ? targetIndex : targetIMG);
+  }
+
   return (
     <div className="carousel">
       <div className="carousel-image" style={style}>
-        <button>{"<"}</button>
+        <button onClick={()=>swapImage(-1)}>{"<"}</button>
 
-        <button>{">"}</button>
+        <button onClick={()=>swapImage(1)}>{">"}</button>
       </div>
     </div>
   )
